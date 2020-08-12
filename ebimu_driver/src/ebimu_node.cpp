584d783567147247f50e0031150c1c8f33e4b171
jiohLee <jiosiro@gmail.com>
2020-08-13 07:45:32 +0900

    k-city test version 1.1

--- ebimu_driver/src/ebimu_node.cpp
+++ ebimu_driver/src/ebimu_node.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char** argv)
 {
-    ros::init(argc,argv,"/ebimu_driver");
+    ros::init(argc,argv,"ebimu_driver");
     ebimu e;
     while(ros::ok())
     {
--- localizer/src/localizer_node.cpp
+++ localizer/src/localizer_node.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char** argv)
 {
-    ros::init(argc,argv,"/localizer");
+    ros::init(argc,argv,"localizer");
     Localizer l;
     ros::spin();
     return 0;
